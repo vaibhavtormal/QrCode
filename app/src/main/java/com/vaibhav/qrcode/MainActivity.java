a91a089c15dfdeee8e1cd728aea6bce9ee81cf1a
vaibhav <vaibhavtormal2020@gmail.com>
2024-02-06 11:41:44 +0530

    Qrcode is a cutting-edge UI code scanner app designed to revolutionize the way you interact with QR codes and barcodes. With its intuitive user interface and powerful features, QrCode makes scanning and decoding codes a seamless and efficient experience.
    Key Features:
    
    Intuitive User Interface: Our app boasts a user-friendly interface that ensures effortless navigation and smooth operation. Whether you're a novice or a seasoned user, you'll find QrCode easy to use and highly functional.
    
    Advanced Code Scanning: Equipped with advanced code scanning technology, Qrcode swiftly captures QR codes and barcodes with precision and accuracy. Say goodbye to manual input and hello to instant scanning.

---
+++ app/src/main/java/com/vaibhav/qrcode/MainActivity.java
@@ -0,0 +1,81 @@
+package com.vaibhav.qrcode;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class MainActivity extends AppCompatActivity {
+    TextView email;
+    CodeScanner codeScanner;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 12;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        permissionCheck();
+        CodeScannerView codeScannerView = findViewById(R.id.scanner_view);
+        codeScanner = new CodeScanner(this, codeScannerView);
+        codeScanner.setAutoFocusEnabled(true);
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        email.setText(result.getText().toString());
+                    }
+                });
+            }
+        });
+        codeScannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
+    }
+
+    protected void permissionCheck() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start the code scanner preview
+                codeScanner.startPreview();
+            } else {
+                // Permission denied, you may handle it here
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        codeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        codeScanner.stopPreview();
+    }
+}
